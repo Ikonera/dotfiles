e012db089b19b7bd30aaa9a6abd6e2756521e193
Ikonera <gabrielmlt@protonmail.ch>
2022-07-29 21:51:19 +0200

    WIP role(neovim): change virtual text icon

--- roles/neovim/files/lua/ikonera/lspsaga.lua
+++ roles/neovim/files/lua/ikonera/lspsaga.lua
@@ -1,12 +1,19 @@
 
 -- Lspsaga configuration
 
-require"lspsaga".setup {
+require"lspsaga".setup({
 	use_saga_diagnostic_sign = true,
 	error_sign = "",
 	warn_sign = "",
 	hint_sign = "",
 	infor_sign = "",
 	border_style = "rounded",
-}
+	code_action_icon = "",
+	code_action_prompt = {
+		enable = true,
+		sign = true,
+		sign_priority = 40,
+		virtual_text = true,
+	},
+})
 
