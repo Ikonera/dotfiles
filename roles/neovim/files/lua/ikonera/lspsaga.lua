d0d21480dc8017b0af22d9a47ca8de94078f6a5e
Ikonera <gabrielmlt@protonmail.ch>
2022-07-29 21:24:03 +0200

    role(neovim): add lspsaga glyphs and some style

--- roles/neovim/files/lua/ikonera/lspsaga.lua
+++ roles/neovim/files/lua/ikonera/lspsaga.lua
@@ -2,5 +2,11 @@
 -- Lspsaga configuration
 
 require"lspsaga".setup {
-	-- default config
+	use_saga_diagnostic_sign = true,
+	error_sign = "",
+	warn_sign = "",
+	hint_sign = "",
+	infor_sign = "",
+	border_style = "rounded",
 }
+
