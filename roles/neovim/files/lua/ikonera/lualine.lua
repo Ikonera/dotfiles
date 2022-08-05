e649c5d1721874fddd2deae255922136863e3231
Ikonera <gabrielmlt@protonmail.ch>
2022-08-05 11:10:16 +0200

    Remove some sections in lualine

--- roles/neovim/files/lua/ikonera/lualine.lua
+++ roles/neovim/files/lua/ikonera/lualine.lua
@@ -12,9 +12,9 @@ require"lualine".setup {
 	},
 	sections = {
 		lualine_a = {'mode'},
-		lualine_b = {'branch', 'diff', 'diagnostics'},
+		lualine_b = {'branch', 'diff'},
 		lualine_c = {'filename'},
-		lualine_x = {'lsp_progress', 'encoding', 'fileformat', 'filetype'},
+		lualine_x = {'lsp_progress', 'filetype'},
 		lualine_y = {'progress'},
 		lualine_z = {'location'}
 	},
