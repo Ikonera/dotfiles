6cd7a799561f251d4b6aa344dbc340c67f7de83c
Ikonera <gabrielmlt@protonmail.ch>
2022-08-05 10:14:49 +0200

    WIP: change Telescope mappings

--- roles/neovim/files/lua/ikonera/telescope.lua
+++ roles/neovim/files/lua/ikonera/telescope.lua
@@ -15,7 +15,22 @@ telescope.setup({
 			".idea/",
 			".vscode/",
 		},
-	}
+		vimgrep_arguments = {
+			"rg",
+		},
+	},
+	pickers = {
+		find_files = {
+			n = {
+				["ls"] = function (prompt_bufnr)
+					local selection = require"telescope.actions.state".get_selected_entry()
+					local dir = vim.fn.fnamemodify(selection.path, ":p:h")
+					require"telescope.actions".close(prompt_bufnr)
+					vim.cmd(string.format("silent cd %s", dir))
+				end
+			},
+		},
+	},
 })
 
 telescope.load_extension 'file_browser'
