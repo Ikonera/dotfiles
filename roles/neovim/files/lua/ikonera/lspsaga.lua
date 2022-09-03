a057eaa4575b50cc24f8920f0f025c14ab02b40a
Ikonera <gabrielmlt@protonmail.ch>
2022-09-03 18:21:20 +0200

    feat: goto impl/refs

--- roles/neovim/files/lua/ikonera/lspsaga.lua
+++ roles/neovim/files/lua/ikonera/lspsaga.lua
@@ -15,3 +15,5 @@ vim.api.nvim_set_keymap('n', "R", "<cmd>Lspsaga rename<CR>", { silent = true, no
 vim.api.nvim_set_keymap('n', "gl", "<cmd>Lspsaga show_line_diagnostics<CR>", { silent = true, noremap = true })
 vim.api.nvim_set_keymap('n', "<Space>e", "<cmd>Lspsaga diagnostic_jump_prev<CR>", { silent = true, noremap = true })
 vim.api.nvim_set_keymap('n', "<Space>r", "<cmd>Lspsaga diagnostic_jump_next<CR>", { silent = true, noremap = true })
+vim.api.nvim_set_keymap('n', "gi", "<cmd>lua require('telescope.builtin').lsp_implementations()<CR>", { silent = true, noremap = true })
+vim.api.nvim_set_keymap('n', "gr", "<cmd>lua require('telescope.builtin').lsp_references()<CR>", { silent = true, noremap = true })
