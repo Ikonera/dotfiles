ce2773154ed89f2d140b99a95ea65c8b16f31e77
Ikonera <gabrielmlt@protonmail.ch>
2022-09-03 20:17:46 +0200

    fix: instanciate term without exec zsh again

--- roles/neovim/files/lua/ikonera/floaterm.lua
+++ roles/neovim/files/lua/ikonera/floaterm.lua
@@ -2,6 +2,6 @@
 -- Floaterm
 
 vim.api.nvim_set_keymap('n', "<Space>nn", "<cmd>FloatermNew nnn -de<CR>", { silent = true, noremap = true })
-vim.api.nvim_set_keymap('n', "<Space>z", "<cmd>FloatermNew! --width=0.9 --height=0.9 zsh<CR>", { silent = true, noremap = true })
+vim.api.nvim_set_keymap('n', "<Space>z", "<cmd>FloatermNew! --width=0.9 --height=0.9 <CR>", { silent = true, noremap = true })
 vim.api.nvim_set_keymap('n', "<Space>lg", "<cmd>FloatermNew --width=0.9 --height=0.9 lazygit<CR>", { silent = true, noremap = true })
 vim.api.nvim_set_keymap('n', "<Space>ld", "<cmd>FloatermNew --width=0.9 --height=0.9 lazydocker<CR>", { silent = true, noremap = true })
