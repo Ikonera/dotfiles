6a010263249ff3050eb02493e8cd69d60dde4b29
Ikonera <gabrielmlt@protonmail.ch>
2022-09-03 18:19:52 +0200

    feat(lua): add support for vim dev

--- roles/neovim/files/lua/ikonera/lsp/completion.lua
+++ roles/neovim/files/lua/ikonera/lsp/completion.lua
@@ -3,6 +3,7 @@
 
 local cmp = require'cmp'
 
+
 local has_words_before = function()
 	local line, col = unpack(vim.api.nvim_win_get_cursor(0))
 	return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
@@ -88,6 +89,7 @@ cmp.setup({
 	},
 	sources = cmp.config.sources({
 		{ name = 'nvim_lsp' },
+		{ name = 'nvim_lua' },
 		{ name = 'vsnip' }, -- For vsnip users.
 		-- { name = 'luasnip' }, -- For luasnip users.
 		-- { name = 'ultisnips' }, -- For ultisnips users.
