d44d59e17aff8bcb0414662ba611dab86ae4cce4
Ikonera <gabrielmlt@protonmail.ch>
2022-10-17 11:16:04 +0200

    style(git): replace old signs with nerd font

--- roles/neovim/files/lua/ikonera/gitsigns.lua
+++ roles/neovim/files/lua/ikonera/gitsigns.lua
@@ -5,18 +5,18 @@ local signs = require"gitsigns"
 
 signs.setup({
 	signs = {
-		add          = {hl = 'GitSignsAdd'   , text = '│', numhl='GitSignsAddNr'   , linehl='GitSignsAddLn'},
-		change       = {hl = 'GitSignsChange', text = '│', numhl='GitSignsChangeNr', linehl='GitSignsChangeLn'},
-		delete       = {hl = 'GitSignsDelete', text = '_', numhl='GitSignsDeleteNr', linehl='GitSignsDeleteLn'},
-		topdelete    = {hl = 'GitSignsDelete', text = '‾', numhl='GitSignsDeleteNr', linehl='GitSignsDeleteLn'},
-		changedelete = {hl = 'GitSignsChange', text = '~', numhl='GitSignsChangeNr', linehl='GitSignsChangeLn'},
+		add          = {hl = 'GitSignsAdd'   , text = '⏽', numhl='GitSignsAddNr'   , linehl='GitSignsAddLn'},
+		change       = {hl = 'GitSignsChange', text = '⏽', numhl='GitSignsChangeNr', linehl='GitSignsChangeLn'},
+		delete       = {hl = 'GitSignsDelete', text = '⏽', numhl='GitSignsDeleteNr', linehl='GitSignsDeleteLn'},
+		topdelete    = {hl = 'GitSignsDelete', text = '⏽', numhl='GitSignsDeleteNr', linehl='GitSignsDeleteLn'},
+		changedelete = {hl = 'GitSignsChange', text = '⏽', numhl='GitSignsChangeNr', linehl='GitSignsChangeLn'},
 	},
 	signcolumn = true,
 	current_line_blame = false,
-	-- current_line_blame_opts = {
-	-- 	delay = 0,
-		-- virt_text = true,
-		-- virt_text_pos = "right_align",
-	-- },
+	current_line_blame_opts = {
+		delay = 0,
+		virt_text = true,
+		virt_text_pos = "overlay",
+	},
 	sign_priority = 6,
 })
