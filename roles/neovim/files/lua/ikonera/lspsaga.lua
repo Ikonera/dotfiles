3642f29830cc204aef24c0a5f32d59096e2a9152
Ikonera <gabrielmlt@protonmail.ch>
2022-11-11 19:56:13 +0100

    fix(lsp): definitions will be removed after 3 days...

--- roles/neovim/files/lua/ikonera/lspsaga.lua
+++ roles/neovim/files/lua/ikonera/lspsaga.lua
@@ -10,7 +10,7 @@ vim.api.nvim_set_keymap('n', "gh", "<cmd>Lspsaga hover_doc<CR>", { silent = true
 vim.api.nvim_set_keymap('n', "go", "<cmd>LSoutlineToggle<CR>", { silent = true, noremap = true })
 vim.api.nvim_set_keymap('n', "gf", "<cmd>Lspsaga lsp_finder<CR>", { silent = true, noremap = true })
 vim.api.nvim_set_keymap('n', "ga", "<cmd>Lspsaga code_action<CR>", { silent = true, noremap = true })
-vim.api.nvim_set_keymap('n', "gd", "<cmd>Lspsaga preview_definition<CR>", { silent = true, noremap = true })
+vim.api.nvim_set_keymap('n', "gd", "<cmd>Lspsaga peek_definition<CR>", { silent = true, noremap = true })
 vim.api.nvim_set_keymap('n', "R", "<cmd>Lspsaga rename<CR>", { silent = true, noremap = true })
 vim.api.nvim_set_keymap('n', "gl", "<cmd>Lspsaga show_line_diagnostics<CR>", { silent = true, noremap = true })
 vim.api.nvim_set_keymap('n', "<Space>e", "<cmd>Lspsaga diagnostic_jump_prev<CR>", { silent = true, noremap = true })
