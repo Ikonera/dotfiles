820076fbfb5dde7685710d0f0c28dbec95f4ea03
Ikonera <gabrielmlt@protonmail.ch>
2022-11-11 19:04:25 +0100

    feat(telescope): add new extensions

--- roles/neovim/files/lua/ikonera/telescope.lua
+++ roles/neovim/files/lua/ikonera/telescope.lua
@@ -2,7 +2,6 @@
 -- Telescope config
 
 local telescope = require'telescope'
-local actions = require"telescope.actions"
 
 telescope.setup({
 	defaults = {
@@ -21,6 +20,9 @@ telescope.setup({
 
 telescope.load_extension 'file_browser'
 telescope.load_extension 'project'
+telescope.load_extension 'gh'
+telescope.load_extension 'media_files'
+telescope.load_extension 'repo'
 
 vim.api.nvim_set_keymap('n', "<Space>f", "<cmd>Telescope find_files hidden=true<CR>", { silent = true, noremap = true })
 vim.api.nvim_set_keymap('n', "<Space>g", "<cmd>Telescope live_grep<CR>", { silent = true, noremap = true })
--- roles/neovim/files/lua/plugins.lua
+++ roles/neovim/files/lua/plugins.lua
@@ -73,9 +73,17 @@ return require('packer').startup({function(use)
   }
 
   -- Telescope
-  use {'nvim-telescope/telescope.nvim', requires = { 'nvim-lua/plenary.nvim' } }
-  use { "nvim-telescope/telescope-file-browser.nvim" }
-  use { "nvim-telescope/telescope-project.nvim" }
+  use {
+	  'nvim-telescope/telescope.nvim',
+	  requires = {
+		  'nvim-lua/plenary.nvim',
+		  'nvim-telescope/telescope-github.nvim',
+		  "nvim-telescope/telescope-file-browser.nvim",
+		  "nvim-telescope/telescope-project.nvim",
+		  "nvim-telescope/telescope-media-files.nvim",
+		  "cljoly/telescope-repo.nvim",
+	  }
+  }
 
   -- Others...
   use 'kyazdani42/nvim-web-devicons'
