63c4df51e6becae844471b18475aae75ea7635a0
Ikonera <gabrielmlt@protonmail.ch>
2022-11-12 19:06:07 +0100

    feat(gitsigns): add new git icons

--- roles/neovim/files/lua/ikonera/gitsigns.lua
+++ roles/neovim/files/lua/ikonera/gitsigns.lua
@@ -5,18 +5,19 @@ local signs = require"gitsigns"
 
 signs.setup({
 	signs = {
-		add          = {hl = 'GitSignsAdd'   , text = '⏽', numhl='GitSignsAddNr'   , linehl='GitSignsAddLn'},
-		change       = {hl = 'GitSignsChange', text = '⏽', numhl='GitSignsChangeNr', linehl='GitSignsChangeLn'},
-		delete       = {hl = 'GitSignsDelete', text = '⏽', numhl='GitSignsDeleteNr', linehl='GitSignsDeleteLn'},
-		topdelete    = {hl = 'GitSignsDelete', text = '⏽', numhl='GitSignsDeleteNr', linehl='GitSignsDeleteLn'},
-		changedelete = {hl = 'GitSignsChange', text = '⏽', numhl='GitSignsChangeNr', linehl='GitSignsChangeLn'},
+		add          = { hl = 'GitSignsAdd'   , text = '│', numhl='GitSignsAddNr'   , linehl='GitSignsAddLn'    },
+		change       = { hl = 'GitSignsChange', text = '│', numhl='GitSignsChangeNr', linehl='GitSignsChangeLn' },
+		delete       = { hl = 'GitSignsDelete', text = '│', numhl='GitSignsDeleteNr', linehl='GitSignsDeleteLn' },
+		topdelete    = { hl = 'GitSignsDelete', text = '│', numhl='GitSignsDeleteNr', linehl='GitSignsDeleteLn' },
+		changedelete = { hl = 'GitSignsChange', text = '│', numhl='GitSignsChangeNr', linehl='GitSignsChangeLn' },
+		untracked    = { hl = 'GitSignsAdd'   , text = '│', numhl='GitSignsAddNr'   , linehl='GitSignsAddLn'    },
 	},
 	signcolumn = true,
 	current_line_blame = false,
 	current_line_blame_opts = {
 		delay = 0,
 		virt_text = true,
-		virt_text_pos = "overlay",
+		virt_text_pos = "eol",
 	},
 	sign_priority = 6,
 })
