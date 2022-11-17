878c83c869a9d735f6530df3e641d78deb164e88
Ikonera <gabrielmlt@protonmail.ch>
2022-11-17 11:54:16 +0100

    feat(telescope): add keymap

--- roles/neovim/files/lua/ikonera/telescope.lua
+++ roles/neovim/files/lua/ikonera/telescope.lua
@@ -34,6 +34,7 @@ telescope.load_extension 'media_files'
 telescope.load_extension 'repo'
 
 vim.api.nvim_set_keymap('n', "<Space>f", "<cmd>Telescope find_files hidden=true<CR>", { silent = true, noremap = true })
+vim.api.nvim_set_keymap('n', "fb", "<cmd>Telescope file_browser<CR>", { silent = true, noremap = true })
 vim.api.nvim_set_keymap('n', "<Space>g", "<cmd>Telescope live_grep<CR>", { silent = true, noremap = true })
 vim.api.nvim_set_keymap('n', "<Space>p", "<cmd>Telescope project<CR>", { silent = true, noremap = true })
 vim.api.nvim_set_keymap('n', "<Space>b", "<cmd>Telescope buffers hidden=true<CR>", { silent = true, noremap = true })
